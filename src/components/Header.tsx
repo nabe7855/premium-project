c25da1f2c61553f819a63e75e493ca7e07973147
mori <nabe7855@gmail.com>
2025-06-22 22:06:49 +0900

    fix: remove unused import

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Image from "next/image";
+//import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { Menu, Phone } from "lucide-react";
